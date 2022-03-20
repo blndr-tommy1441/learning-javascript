20da0dbd23feafb4c90d480c8a7f4048e79af6c0
blndr-tommy1441 <t.helmn25@gmail.com>
2022-03-20 14:53:51 +0100

    modified the strings.js

--- src/scripts/strings.js
+++ src/scripts/strings.js
@@ -7,3 +7,29 @@ let surname = 'Helmn';
 console.log(surname);
 
 console.log(firstName + " " + surname);
+
+
+
+
+// ESCAPING CHARACTER
+
+/* To escape any special character i.e. the single quote or the double quotes, we use a backslash BEFORE each character, or mix quotes
+*/
+
+/*
+ERROR
+let char1 = "string of character "without" using the escaped character"
+NOTE: the above throws an error as there's two sets of double quotes
+*/
+
+// correct way number 1
+let char2 = "Correct way number 1 to \"escape\" a character";
+console.log(char2);
+
+// correct way number 2 (mixing quotes)
+let char3 = "Correct way number 2 to 'escape' a character";
+console.log(char3);
+
+// correct way number 3 (mixing quotes)
+let char4 = 'Correct way number 3 to "escape" a character';
+console.log(char4);
