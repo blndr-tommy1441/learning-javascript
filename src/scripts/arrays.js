1bdb33e341cc73cd25582fc94a46b6c39c7044dc
blndr-tommy1441 <t.helmn25@gmail.com>
2022-04-05 13:30:24 +0200

    working with arrays

--- src/scripts/arrays.js
+++ src/scripts/arrays.js
@@ -16,3 +16,42 @@ console.log(arrayName[1]);
 
 let nestedArray = [["string", 324234, 45352], [3424, 545, 2323]];
 console.log(nestedArray[1][1]);
+
+// Adding to an Array...Things to remember
+// The value will always be added to the end of the array
+
+let seassons;
+seassons = ["Winter", "Spring", "Summer"];
+console.log(seassons);
+
+seassons.push("Fall");
+console.log(seassons);
+
+
+// Remove from an Array...Things to remember
+// The value will always be removed from the end of the array
+
+let seasson;
+seasson = seassons;
+console.log(seasson);
+
+seasson.pop();
+console.log(seasson);
+
+// the advantage is that we can assign the removed value in a variable
+let removedValue = seasson.pop();
+console.log(removedValue);
+
+// Use splice(arrayIndex, number of elements to remove) to remove arbitrary item.
+// Use shift() to remove from beginning.
+// Use pop() to remove from end.
+
+// TEST
+let testArray = ["a", "b", "c", "d"];
+console.log(testArray);
+testArray.push("e");
+console.log(testArray);
+// here we remove the second element of the array (index number 1)
+// and the amount of elements removed (this case 1 element)
+testArray.splice(1, 1);
+console.log(testArray);
